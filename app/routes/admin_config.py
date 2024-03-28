55710bc1c471782d4f51bd0cd1af901f8b0a7822
Muhammad Inam <m.inam@isbei.com>
2024-03-28 05:26:57 +0500

    admin config endpoint updated

--- app/routes/admin_config.py
+++ app/routes/admin_config.py
@@ -8,17 +8,17 @@ from app.schemas.requests.admin_config import UpdateAdminConfigRequest
 from app.services import admin_config
 
 admin_config_router_protected = APIRouter(
-    prefix="/admin/config",
+    prefix="/admin",
     tags=["Admin"],
     responses={404: {"description": "Not found"}},
     dependencies=[Depends(oauth2_scheme), Depends(validate_access_token), Depends(is_admin)]
 )
 
-@admin_config_router_protected.put("/", status_code=status.HTTP_200_OK)
+@admin_config_router_protected.put("/config", status_code=status.HTTP_200_OK)
 async def update_admin_config(data: UpdateAdminConfigRequest, session: Session = Depends(get_session)):
     await admin_config.update_admin_config(data, session)
     return JSONResponse({"message": "Admin config has been updated"})
 
-@admin_config_router_protected.get("/", status_code=status.HTTP_200_OK, response_model=AdminConfigResponse)
+@admin_config_router_protected.get("/config", status_code=status.HTTP_200_OK, response_model=AdminConfigResponse)
 async def get_admin_config(session: Session = Depends(get_session)):
     return await admin_config.get_admin_config(session)
