047401bed516f95dfecf7fa95bfad6e10645e045
Muhammad Inam <m.inam@isbei.com>
2024-04-19 04:30:53 +0500

    updated admin config request schema

--- app/schemas/requests/admin_config.py
+++ app/schemas/requests/admin_config.py
@@ -6,6 +6,7 @@ class UpdateAdminConfigRequest(BaseModel):
     llm_temperature: Optional[float] = None
     llm_prompt: Optional[str] = None
     llm_role: Optional[str] = None
+    llm_streaming: Optional[bool] = None
     greeting_message: Optional[str] = None
     disclaimers: Optional[str] = None
     gdrive_enabled: Optional[bool] = None
