9b11f504c8dcfd48462f7c9f5d77bd81f918610f
m-inam786 <120548528+m-inam786@users.noreply.github.com>
2024-12-18 21:03:31 +0500

    fix

--- app/services/user_document.py
+++ app/services/user_document.py
@@ -25,8 +25,8 @@ async def manage_upload_file(files, username, session):
     if person_data.role == Role.User:
         max_files_allowed = settings.FREE_PLAN_FILE_UPLOAD_LIMIT if person_data.role == Role.User == Plan.free else settings.PREMIUM_PLANS_FILE_UPLOAD_LIMIT
     user_doc_count = session.query(UserDocument).filter(UserDocument.user_id == username).all()
-    if user_doc_count > max_files_allowed:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"You can't upload further files Only {max_files_allowed - user_doc_count} more files can be uploaded.")
+    if len(user_doc_count) > max_files_allowed:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"You can't upload further files Only {max_files_allowed - len(user_doc_count)} more files can be uploaded.")
 
     base_directory = Path('/app/static/') / str(person_data.id)
     # Create the directory for the user if it does not exist
